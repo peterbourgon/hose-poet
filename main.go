f9af4f6cce7d23bb61c7a80978da811c7e4795ed
Peter Bourgon <peter@bourgon.org>
2013-02-09 12:04:31 +0100

    Modularize the Stream

--- main.go
+++ main.go
@@ -2,22 +2,11 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"io"
-	"io/ioutil"
 	"log"
-	"os"
-	"strings"
-
-	"github.com/mrjones/oauth"
 )
 
 var (
-	credentials    = flag.String("credentials", "CREDENTIALS", "credentials cache file")
-	consumerKey    = flag.String("consumer.key", "", "consumer key")
-	consumerSecret = flag.String("consumer.secret", "", "consumer secret")
-	accessToken    = flag.String("access.token", "", "access token")
-	accessSecret   = flag.String("access.secret", "", "access token secret")
+	credentials = flag.String("credentials", "CREDENTIALS", "credentials cache file")
 )
 
 func init() {
@@ -25,76 +14,21 @@ func init() {
 }
 
 func main() {
-	if buf, err := ioutil.ReadFile(*credentials); err == nil {
-		if toks := strings.Split(string(buf), "\n"); len(toks) >= 4 {
-			*consumerKey = toks[0]
-			*consumerSecret = toks[1]
-			*accessToken = toks[2]
-			*accessSecret = toks[3]
-		}
+	stream, err := NewStream(*credentials)
+	if err != nil {
+		log.Println(err)
+		log.Println("To create a credentials file,")
+		log.Println(" go get github.com/peterbourgon/hose-poet/make-credentials")
+		log.Println("then run make-credentials")
+		log.Fatal("")
 	}
 
-	if *consumerKey == "" || *consumerSecret == "" {
-		log.Fatal("-consumer.key and -consumer.secret required")
-	}
-
-	c := oauth.NewConsumer(
-		*consumerKey,
-		*consumerSecret,
-		oauth.ServiceProvider{
-			RequestTokenUrl:   "http://api.twitter.com/oauth/request_token",
-			AuthorizeTokenUrl: "https://api.twitter.com/oauth/authorize",
-			AccessTokenUrl:    "https://api.twitter.com/oauth/access_token",
-		},
-	)
-
-	if *accessToken == "" || *accessSecret == "" {
-		requestToken, url, err := c.GetRequestTokenAndUrl("oob")
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Println("(1) Go to: " + url)
-		fmt.Println("(2) Grant access, you should get back a verification code.")
-		fmt.Println("(3) Enter that verification code here: ")
-		var verificationCode string
-		fmt.Scanln(&verificationCode)
-
-		tok, err := c.AuthorizeToken(requestToken, verificationCode)
-		if err != nil {
-			log.Fatal(err)
-		}
-		*accessToken, *accessSecret = tok.Token, tok.Secret
-		log.Printf("OK, got authorize token")
-	}
-
-	if *consumerKey == "" || *consumerSecret == "" || *accessToken == "" || *accessSecret == "" {
-		if err := ioutil.WriteFile(
-			*credentials,
-			[]byte(strings.Join(
-				[]string{
-					*consumerKey,
-					*consumerSecret,
-					*accessToken,
-					*accessSecret,
-				},
-				"\n",
-			)),
-			0600,
-		); err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	response, err := c.Get(
-		"https://stream.twitter.com/1.1/statuses/sample.json",
-		map[string]string{},
-		&oauth.AccessToken{*accessToken, *accessSecret},
-	)
+	tweets, err := stream.Consume()
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer response.Body.Close()
 
-	io.Copy(os.Stdout, response.Body)
+	for tweet := range tweets {
+		log.Printf("%20s: %s", tweet.User.ScreenName, tweet.Text)
+	}
 }
---
+++ stream.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"github.com/mrjones/oauth"
+	"io/ioutil"
+	"log"
+	"strings"
+)
+
+type Stream struct {
+	ConsumerKey    string
+	ConsumerSecret string
+	AccessToken    string
+	AccessSecret   string
+}
+
+func NewStream(credentialsFile string) (*Stream, error) {
+	buf, err := ioutil.ReadFile(credentialsFile)
+	if err != nil {
+		return nil, err
+	}
+
+	toks := strings.Split(strings.TrimSpace(string(buf)), "\n")
+	if len(toks) < 4 {
+		return nil, fmt.Errorf("invalid credentials file format")
+	}
+
+	return &Stream{
+		ConsumerKey:    toks[0],
+		ConsumerSecret: toks[1],
+		AccessToken:    toks[2],
+		AccessSecret:   toks[3],
+	}, nil
+}
+
+type Tweet struct {
+	ID   uint64 `json:"id"`
+	User struct {
+		ID         uint64 `json:"id"`
+		ScreenName string `json:"screen_name"`
+	} `json:"user"`
+	Text string `json:"text"`
+}
+
+func (s *Stream) Consume() (chan Tweet, error) {
+	c := oauth.NewConsumer(
+		s.ConsumerKey,
+		s.ConsumerSecret,
+		oauth.ServiceProvider{
+			RequestTokenUrl:   "http://api.twitter.com/oauth/request_token",
+			AuthorizeTokenUrl: "https://api.twitter.com/oauth/authorize",
+			AccessTokenUrl:    "https://api.twitter.com/oauth/access_token",
+		},
+	)
+
+	response, err := c.Get(
+		"https://stream.twitter.com/1.1/statuses/sample.json",
+		map[string]string{},
+		&oauth.AccessToken{
+			Token:  s.AccessToken,
+			Secret: s.AccessSecret,
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	tweets := make(chan Tweet)
+	go func() {
+		defer response.Body.Close()
+		r := bufio.NewReader(response.Body)
+		for {
+			buf, err := r.ReadBytes('\n')
+			if err != nil {
+				log.Print(err)
+				break
+			}
+
+			var tweet Tweet
+			err = json.Unmarshal(buf, &tweet)
+			if err != nil {
+				continue
+			}
+			if tweet.ID == 0 {
+				continue
+			}
+			tweets <- tweet
+		}
+	}()
+	return tweets, nil
+}
